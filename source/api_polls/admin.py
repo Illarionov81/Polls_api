d6012e09d3e63393f2b5e6ae59450869e85055bc
alex <nuvorish81@gmail.com>
2021-06-16 20:04:19 +0600

    Added Models for polls, questions and users answers also define admin

--- source/api_polls/admin.py
+++ source/api_polls/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from api_polls.models import Polls, Questions
+
+
+class QuestionsInline(admin.TabularInline):
+    model = Questions
+    fields = ['poll', 'text', 'questions_type']
+
+
+class PollsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'start_date', 'finish_date']
+    list_filter = ['name']
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'start_date']
+    fields = ['name', 'finish_date', 'description']
+    inlines = [QuestionsInline]
+
+
+class QuestionsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'poll', 'text', 'questions_type']
+    list_display_links = ['id', 'poll', 'text']
+    list_filter = ['id']
+    search_fields = ['id', 'poll']
+    fields = ['poll', 'text', 'questions_type']
+
+
+admin.site.register(Polls, PollsAdmin)
+admin.site.register(Questions, QuestionsAdmin)
--- source/api_polls/models.py
+++ source/api_polls/models.py
@@ -1,3 +1,79 @@
 from django.db import models
 
-# Create your models here.
+
+QUESTION_TYPE_TEXT = 'answer with text'
+QUESTION_TYPE_CHOICE_ONE = 'choice one answer'
+QUESTION_TYPE_CHOICE_MANY = 'multiple choice answer'
+QUESTION_TYPE_CHOICES = (
+    (QUESTION_TYPE_TEXT, 'answer with text'),
+    (QUESTION_TYPE_CHOICE_ONE, 'choice one answer'),
+    (QUESTION_TYPE_CHOICE_MANY, 'multiple choice answer')
+)
+
+
+class Polls(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Poll")
+    start_date = models.DateTimeField(auto_now_add=True, verbose_name='Start time')
+    finish_date = models.DateTimeField(verbose_name='Finish time', blank=True)
+    description = models.TextField(max_length=3000, verbose_name='Description')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Poll'
+        verbose_name_plural = 'Polls'
+
+
+class Questions(models.Model):
+    poll = models.ForeignKey('api_polls.Polls', verbose_name='Poll', related_name='questions',
+                             on_delete=models.CASCADE)
+    text = models.TextField(max_length=2500, verbose_name='Text')
+    questions_type = models.CharField(max_length=35, choices=QUESTION_TYPE_CHOICES, default=QUESTION_TYPE_TEXT,
+                                      verbose_name='Type')
+
+    def __str__(self):
+        return f'{self.text}'
+
+    class Meta:
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
+
+
+class UsersTextAnswers(models.Model):
+    question = models.ForeignKey('api_polls.Questions', verbose_name='question', related_name='User_answer',
+                                 on_delete=models.CASCADE)
+    answer = models.TextField(max_length=3500, verbose_name='Users text answer')
+    user_id = models.IntegerField(verbose_name='User id')
+
+    def __str__(self):
+        return f'{self.answer}'
+
+    class Meta:
+        verbose_name = 'Users Answer'
+        verbose_name_plural = 'Users Answers'
+
+
+# class ChoiceOneAnswers(models.Model):
+#     question = models.ForeignKey('api_polls.Questions', verbose_name='question', related_name='Choice one answer',
+#                                  on_delete=models.CASCADE)
+#     answer = models.OneToOneField('api_polls.PreparedAnswers', )
+#     user_id = models.IntegerField(verbose_name='User id')
+#
+#     def __str__(self):
+#         return f'{self.answer}'
+#
+#     class Meta:
+#         verbose_name = 'UsersAnswer'
+#         verbose_name_plural = 'UsersAnswers'
+#
+#
+# class PreparedAnswers(models.Model):
+#     answer = models.TextField(max_length=3500, verbose_name='prepared answer')
+#
+#     def __str__(self):
+#         return f'{self.answer}'
+#
+#     class Meta:
+#         verbose_name = 'Prepared Answer'
+#         verbose_name_plural = 'Prepared Answers'
